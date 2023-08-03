be447d7bbf47625ddb0c51c7a3229fdb42003aeb
李彦杰 <liyanjie@liyanjiedeMacBook-Pro.local>
2023-08-03 15:24:09 +0800

    add version

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,27 @@
 package cmd
 
 import (
-	"errors"
+	"fmt"
 	"github.com/spf13/cobra"
 )
 
+var verbose bool
+
 var RootCmd = &cobra.Command{
 	Use:               "stool",
-	Short:             "stool shell工具",
-	Long:              "stool shell工具",
+	Short:             "stool: shell小工具，用于ssh密钥生成、免密配置、shell命令执行",
+	Long:              "stool: shell小工具，用于ssh密钥生成、免密配置、shell命令执行",
 	CompletionOptions: cobra.CompletionOptions{DisableDefaultCmd: true},
-	RunE: func(cmd *cobra.Command, args []string) error {
-		return errors.New("no flags find")
+	Run: func(cmd *cobra.Command, args []string) {
+		// 在Run函数中根据-v参数的值输出详细信息
+		if verbose {
+			fmt.Println("stool version: v1.0.0")
+			return
+		}
+		_ = cmd.Help()
 	},
+}
+
+func init() {
+	RootCmd.PersistentFlags().BoolVarP(&verbose, "version", "v", false, "show stool version")
 }
